8ca792c4d4e98fd45c8af6935994235c45ba3594
clevelandjosh <33519616+clevelandjosh@users.noreply.github.com>
2025-04-13 20:31:54 +0000

    Update starthere.sh

--- starthere.sh
+++ starthere.sh
@@ -7,6 +7,13 @@
 # From there we will use those roles, so once Step 0 is done, you will have another manual step to put the new roles secrets into Github. 
 
 # Step 0
+# We will create a management group beneath the initial Root
+
+# The we will create one more layer for our project. You can build out the rest of it as you see fit later. I just want a place to start storing terraform state so I can start building roles.
+
+# We are going to have a few statefiles, one for roles, one for infra, and one for services. This is a standalone greenfield tenant. 
+# If you don't like the layout, thats fine, change it how you see fit, but know the scripts may depend on the layout I used, so....
+
 # For your Azure Environment you will need a bunch of roles.
 # We are assuming you are Global Admin for this Tenant.
 # Lets create the roles. 
